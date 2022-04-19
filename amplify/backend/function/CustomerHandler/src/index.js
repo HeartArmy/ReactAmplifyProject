c4bad000cc3294f8b568c0c51e480650b06ddde2
Mohammed <jahinarham2@icloud.com>
2022-04-19 21:52:11 +0400

    adding files

---
+++ amplify/backend/function/CustomerHandler/src/index.js
@@ -0,0 +1,16 @@
+
+exports.handler = async (event) => {
+    console.log(event) // to help with testing
+    const customerId = event.pathParameters.customerId;
+    const customer = {'customerId': customerId, 'customerName': "Customer " + customerId };
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+     headers: {
+         "Access-Control-Allow-Origin": "*",
+         "Access-Control-Allow-Headers": "*"
+     }, 
+        body: JSON.stringify(customer),
+    };
+    return response;
+};
